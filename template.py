c2b6bc083ef604a873e68a34e2ca966650e0b0cc
Colton Pauderis <colton.pauderis@gmail.com>
2019-12-01 13:22:30 -0500

    template update

--- template.py
+++ template.py
@@ -11,32 +11,20 @@ from util import *
 YEAR = <year>
 DAY = <day>
 
-# TODO: how smart to make this?
-rows = input_rows(DAY, year=YEAR)
-
-(kind, rows, max_row, min_row, _x) = analyze_input(rows)
-print("  Kind: %s" % kind)
-print("n rows: %4d" % rows)
-if min_row == max_row:
-  print("n cols: %4d" % (max_row))
-else:
-  print("n cols: %d-%d" % (min_row, max_row))
-print('.' * 16)
-print('')
+iobj = Input.for_date(DAY, year=YEAR, test=False)
+iobj.pp_analyze()
+rows = list(iobj.rows)
 
 
-
-
-
-def part1(s):
+def part1(rows, iobj):
 
   return
 
 
-def part2(s):
+def part2(rows, iobj):
 
   return
 
 
-print('P1', part1(rows))
-print('P2', part2(rows))
+print('P1', part1(rows, iobj))
+print('P2', part2(rows, iobj))
